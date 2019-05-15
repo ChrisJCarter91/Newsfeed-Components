b4700708ca348b6964ac45a390a3d34eed7727c5
chris.j.carter <chris.j.carter91@gmail.com>
2019-05-15 16:25:08 -0600

    Need to debug Expand/Shrink

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,24 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', (event) => this.expandArticle());
   }
 
-  expandArticle() {
+  expandArticle(event) {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    this.domElement.classList.toggle('article-open');
 
+    if (this.domElement.classList.contains('article-open')) {
+      this.expandButton.textContent = 'expand';
+    } else {
+      this.expandButton.textContent = 'shrink';
+    }
   }
 }
 
@@ -26,4 +32,8 @@ class Article {
 
 */
 
-let articles;
+let articles = Array.from(document.querySelectorAll('.article'));
+
+articles.forEach(articles => {
+  const articleInsance = new Article(articles)
+})
